7c6356889b0f6a9a196db18be9d97899feb9b893
LukeGent <lucgent@gmail.com>
2018-05-09 14:25:44 +0200

    Esercizio 1 fatto

--- TestFinaleLuca/Program.cs
+++ TestFinaleLuca/Program.cs
@@ -7,6 +7,33 @@ using System.Threading.Tasks;
 namespace TestFinaleLuca {
 	class Program {
 		static void Main(string[] args) {
+			int y = 0;
+			int x = 0;
+			bool stop = true;
+			for(int n = 1; stop; n++){
+				if(n % 2 == 0){
+					Console.Write($"{n}, ");
+					++y;
+					if(y >= 15){
+						stop = false;
+						x = n;
+					}
+				}
+			}
+			Console.WriteLine("");
+			Console.WriteLine("Fine numeri Pari");
+			stop = true;
+			for (int n = x; stop; n--) {
+				if (n % 2 != 0) {
+					Console.Write($"{n}, ");
+					--y;
+					if (n == 1) {
+						stop = false;
+					}
+				}
+			}
+			Console.WriteLine("");
+			Console.WriteLine("Fine numeri Dispari");
 		}
 	}
 }
