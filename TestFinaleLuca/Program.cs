54d071c61384882288ce0c56da72ce786483ddd5
LukeGent <lucgent@gmail.com>
2018-05-09 16:10:06 +0200

    Esercizio 2 fatto

--- TestFinaleLuca/Program.cs
+++ TestFinaleLuca/Program.cs
@@ -8,7 +8,42 @@ namespace TestFinaleLuca {
 	class Program {
 		static void Main(string[] args) {
 			StampaNumeri();
+			Console.WriteLine();
+			OccorrenzeParole();
+			Console.WriteLine();
 		}
+
+		static void OccorrenzeParole(){
+			string start = "Questa è una frase di esempio come un'altra frase di esempio";
+			string frase = start;
+			List<string> parole = new List<string>();
+			int index = 0;
+			int occorrenze = 1;
+			int s1 = 0;
+			while(frase.Length > 0 && frase.IndexOf(" ") > 0) {
+				s1 = frase.IndexOf(" ");
+				string str = frase.Substring(index, s1 - index);
+				parole.Add(str);
+				frase = frase.Substring(s1 - index + 1);
+			}
+			parole.Add(frase);
+			List<string> confronta = new List<string>();
+			for(int t = 0; t < parole.Count; t++){
+				confronta.Add(parole[t]);
+			}
+			for (int k = 0; k < confronta.Count; k++){
+				for (int j = k + 1; j < confronta.Count; j++) {
+					if(confronta[k] == confronta[j]){
+						++occorrenze;
+					}
+				}
+				if(occorrenze >= 2){
+					Console.WriteLine($"la parola \"{parole[k]}\" si ripete {occorrenze} volte");
+				}
+				occorrenze = 1;
+			}		
+		}
+
 		static void StampaNumeri(){
 			int y = 0;
 			int x = 0;
