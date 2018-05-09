cdf1329018cc84454b7d2291cfb2fc06b2e4e7d4
LukeGent <lucgent@gmail.com>
2018-05-09 17:34:07 +0200

    mock ok per il pranzo

--- TestFinaleLucaMVC/Controllers/HomeController.cs
+++ TestFinaleLucaMVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestFinaleLucaMVC.Models;
 
 namespace TestFinaleLucaMVC.Controllers {
 	public class HomeController : Controller {
@@ -20,6 +21,21 @@ namespace TestFinaleLucaMVC.Controllers {
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+
+		public ActionResult Pranzo() {
+			ViewBag.Messagge = "Benvenuto nella pagina per aggiungere il pranzo";
+			return View("Pranzo");
+		}
+		[HttpPost]
+		public ActionResult Pranzo(string primo, string secondo, string contorno, string dolce) {
+			DomainModel dm = new DomainModel();
+			dm.AddPranzo(primo, secondo, contorno, dolce);
+			ViewBag.primo = primo;
+			ViewBag.secondo = secondo;
+			ViewBag.contorno = contorno;
+			ViewBag.dolce = dolce;
+			return View("Pranzo");
 		}
 	}
 }
---
+++ TestFinaleLucaMVC/Models/DomainModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestFinaleLucaMVC.Models {
+	public class DomainModel {
+		public void AddPranzo(string primo, string secondo, string contorno, string dolce) {
+		
+		}
+	}
+}
